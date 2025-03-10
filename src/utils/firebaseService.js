edff8a04b89929dd4c1e6a10c4eb8da6e51e87e3
Pablo <barriuso.140174@e.unavarra.es>
2025-03-10 12:54:55 +0100

    firebase setup

--- src/utils/firebaseService.js
+++ src/utils/firebaseService.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getDatabase, ref, onValue } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC_A5eEcpqEtM4GDgneiGcmGp5IBwO1vjU",
-  authDomain: "dsm-webapp-2025.firebaseapp.com",
-  databaseURL: "https://dsm-webapp-2025-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "dsm-webapp-2025",
-  storageBucket: "dsm-webapp-2025.firebasestorage.app",
-  messagingSenderId: "856662678826",
-  appId: "1:856662678826:web:5dd57c819b58ceb2c5d253"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
