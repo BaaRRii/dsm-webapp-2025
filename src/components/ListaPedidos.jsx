93bf49f8fa164390722ab1c990f003317e2de9b9
Pablo <barriuso.140174@e.unavarra.es>
2025-03-23 13:54:59 +0100

    added confirmation modal for deleting orders in ListaPedidos

--- src/components/ListaPedidos.jsx
+++ src/components/ListaPedidos.jsx
@@ -8,16 +8,27 @@ import "./ListaPedidos.css";
 function ListaPedidos() {
   const { user } = useContext(AuthContext);
   const [pedidos, setPedidos] = useState([]);
+  const [mostrarConfirmacion, setMostrarConfirmacion] = useState(false);
+  const [pedidoAEliminar, setPedidoAEliminar] = useState(null);
 
-  const handleBorrarPedido = async (id) => {
+  const handleConfirmar = async () => {
     try {
-      deleteOrder(user.uid, id);
-      const pedidosActualizados = pedidos.filter((pedido) => pedido.id !== id);
+      deleteOrder(user.uid, pedidoAEliminar);
+      const pedidosActualizados = pedidos.filter(
+        (pedido) => pedido.id !== pedidoAEliminar
+      );
       setPedidos(pedidosActualizados);
+      setMostrarConfirmacion(false);
+      setPedidoAEliminar(null);
     } catch (error) {
       console.error("Error al borrar el pedido:", error);
     }
-  }
+  };
+
+  const handleBorrarPedido = (id) => {
+    setPedidoAEliminar(id);
+    setMostrarConfirmacion(true);
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -33,61 +44,87 @@ function ListaPedidos() {
   }, []);
 
   return (
-    <div className="lista-pedidos">
-      {pedidos.length === 0 ? (
-        <p>No tienes pedidos realizados.</p>
-      ) : (
-        <>
-          {pedidos.map((pedido) => (
-            <div key={pedido.id} className="pedido-item">
-              <h3>
-                Pedido realizado el: {new Date(pedido.fecha).toLocaleString()}
-              </h3>
-              <div className="pedido-usuario">
-                <h4>Datos del Usuario:</h4>
-                <p>
-                  <strong>Nombre:</strong> {pedido.usuario.nombre}
-                </p>
-                <p>
-                  <strong>Apellido:</strong> {pedido.usuario.apellido}
-                </p>
-                <p>
-                  <strong>Dirección:</strong> {pedido.usuario.direccion}
-                </p>
-              </div>
-              <div className="pedido-productos">
-                <h4>Productos:</h4>
-                <ul>
-                  {pedido.productos.map((producto, index) => (
-                    <div key={index} className="producto-item">
-                      <p>
-                        <strong>Nombre:</strong> {producto.nombre}
-                      </p>
-                      <p>
-                        <strong>Descripción:</strong> {producto.descripcion}
-                      </p>
-                      <p>
-                        <strong>Cantidad:</strong> {producto.cantidad}
-                      </p>
-                      <p>
-                        <strong>Precio:</strong> {producto.precio}€
-                      </p>
-                    </div>
-                  ))}
-                </ul>
-              </div>
-              <p>
-                <strong>Total:</strong> {pedido.total}€
-              </p>
-              <button className="btn-borrar" onClick={() => handleBorrarPedido(pedido.id)}>
-                <Trash2 size={16} />
-                BORRAR
+    <>
+      {mostrarConfirmacion && (
+        <div className="modal-overlay">
+          <div className="modal-confirmacion">
+            <span>Se va a eliminar tu pedido.</span>
+            <br></br>
+            <br></br>
+            <div className="botones">
+              <button className="btn-confirmar" onClick={handleConfirmar}>
+                Confirmar
+              </button>
+              <button
+                className="btn-confirmar"
+                onClick={() => setMostrarConfirmacion(false)}
+              >
+                Cerrar
               </button>
             </div>
-          ))}
-        </>
+          </div>
+        </div>
       )}
-    </div>
+
+      <div className="lista-pedidos">
+        {pedidos.length === 0 ? (
+          <p>No tienes pedidos realizados.</p>
+        ) : (
+          <>
+            {pedidos.map((pedido) => (
+              <div key={pedido.id} className="pedido-item">
+                <h3>
+                  Pedido realizado el: {new Date(pedido.fecha).toLocaleString()}
+                </h3>
+                <div className="pedido-usuario">
+                  <h4>Datos del Usuario:</h4>
+                  <p>
+                    <strong>Nombre:</strong> {pedido.usuario.nombre}
+                  </p>
+                  <p>
+                    <strong>Apellido:</strong> {pedido.usuario.apellido}
+                  </p>
+                  <p>
+                    <strong>Dirección:</strong> {pedido.usuario.direccion}
+                  </p>
+                </div>
+                <div className="pedido-productos">
+                  <h4>Productos:</h4>
+                  <ul>
+                    {pedido.productos.map((producto, index) => (
+                      <div key={index} className="producto-item">
+                        <p>
+                          <strong>Nombre:</strong> {producto.nombre}
+                        </p>
+                        <p>
+                          <strong>Descripción:</strong> {producto.descripcion}
+                        </p>
+                        <p>
+                          <strong>Cantidad:</strong> {producto.cantidad}
+                        </p>
+                        <p>
+                          <strong>Precio:</strong> {producto.precio}€
+                        </p>
+                      </div>
+                    ))}
+                  </ul>
+                </div>
+                <p>
+                  <strong>Total:</strong> {pedido.total}€
+                </p>
+                <button
+                  className="btn-borrar"
+                  onClick={() => handleBorrarPedido(pedido.id)}
+                >
+                  <Trash2 size={16} />
+                  BORRAR
+                </button>
+              </div>
+            ))}
+          </>
+        )}
+      </div>
+    </>
   );
 }
 
